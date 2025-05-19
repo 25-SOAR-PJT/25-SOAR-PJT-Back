cd8e713a9f6becf48cd93648c432894a629ca8a2
chaewon02 <glgl246@naver.com>
2025-05-19 16:08:36 +0900

    feat: ApiResponse, BaseTimeEntity 기본 세팅

---
+++ src/main/java/org/project/soar/global/abstracts/BaseTimeEntity.java
@@ -0,0 +1,30 @@
+package org.project.soar.global.abstracts;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+	@CreatedDate
+	@JsonIgnore
+	@Column(name = "create_date")
+	private LocalDateTime createDate;
+
+	@LastModifiedDate
+	@JsonIgnore
+	@Column(name = "update_date")
+	private LocalDateTime updateDate;
+}
