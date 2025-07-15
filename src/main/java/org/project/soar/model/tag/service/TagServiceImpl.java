f302bd242b81b8ad89eddf42d7e3c487ebc57654
jieun song <0scarl1216@mju.ac.kr>
2025-07-15 19:32:14 +0900

    feat : tag 추가
    - 추가지원조건, 소득구간 관련 tag 추가

--- src/main/java/org/project/soar/model/tag/service/TagServiceImpl.java
+++ src/main/java/org/project/soar/model/tag/service/TagServiceImpl.java
@@ -37,37 +37,37 @@ public class TagServiceImpl implements TagService{
 
     @Override
     public List<TagResponse> setTagList() {
-        if (fieldRepository.findAll().size() == 0){
+        if (fieldRepository.findAll().isEmpty()){
             fieldService.setFieldList();
         }
         List<Field> fields = fieldRepository.findAll();
-        List<Tag> exitedTags = tagRepository.findAll();
-        if (exitedTags.size() == 0) {
-            List<String> tags_job = List.of("취_창업 컨설팅", "면접지원", "멘토_멘티", "인턴십&연수&공고", "창업 자금 지원", "기타");
-            List<String> tags_house = List.of("신혼부부주거지원", "청년주거지원", "여성주거지원", "기타");
-            List<String> tags_study = List.of("주거 교육", "안전 교육", "예술 교육", "IT_마케팅 교육", "인문 교육", "요리 교육", "취_창업 교육", "금융 교육", "독서실 지원", "응시료&수강료 지원", "장학금&학자금 제출", "기타");
-            List<String> tags_care = List.of("문화행사", "심리상담", "의료지원", "공간지원", "물품지원", "소상공인지원", "장애인지원", "양육지원", "교통비지원", "병역지원", "기타");
-            List<String> tags_familySit = List.of("저소득", "장애인", "한부모_조손", "다자녀", "다문화_탈북민", "보훈대상자");
-            List<String> tags_employed = List.of("재직자", "자영업자", "미취업자", "프리랜서", "일용근로자", "(예비)창업자", "단기근로자", "영농종사자", "제한없음");
-            List<String> tags_ages = List.of("19-24", "25-29", "30-34", "35-39");
 
-            List<List> array_tags = List.of(tags_job, tags_house, tags_study, tags_care, tags_familySit, tags_employed, tags_ages);
+        tagRepository.deleteAll();
 
-            for (int i = 0; i < array_tags.size(); i++) {
-                List<String> tags = array_tags.get(i);
-                Field field = fields.get(i);
+        List<String> tags_job = List.of("취_창업 컨설팅", "면접지원", "멘토_멘티", "인턴십&연수&공고", "창업 자금 지원", "기타");
+        List<String> tags_house = List.of("신혼부부주거지원", "청년주거지원", "여성주거지원", "기타");
+        List<String> tags_study = List.of("주거 교육", "안전 교육", "예술 교육", "IT_마케팅 교육", "인문 교육", "요리 교육", "취_창업 교육", "금융 교육", "독서실 지원", "응시료&수강료 지원", "장학금&학자금 제출", "기타");
+        List<String> tags_care = List.of("문화행사", "심리상담", "의료지원", "공간지원", "물품지원", "소상공인지원", "장애인지원", "양육지원", "교통비지원", "병역지원", "기타");
+        List<String> tags_familySit = List.of("저소득", "장애인", "한부모_조손", "다자녀", "다문화_탈북민", "보훈대상자");
+        List<String> tags_employed = List.of("재직자", "자영업자", "미취업자", "프리랜서", "일용근로자", "(예비)창업자", "단기근로자", "영농종사자", "제한없음");
+        List<String> tags_ages = List.of("19-24", "25-29", "30-34", "35-39");
+        List<String> tags_additional = List.of("지역인재", "중소기업", "기혼자", "군인", "농업인", "한부모가정", "기초생활수급자", "장애인");
+        List<String> tags_income = List.of("기초생활수급자","무소득 또는 월 50만원 이하","월 소득 50~150만원","월소득 150~250만원", "기타","제한없음");
 
-                List<Tag> result = tags.stream()
-                        .map(tag -> new Tag(tag, field))
-                        .collect(Collectors.toList());
+        List<List> array_tags = List.of(tags_job, tags_house, tags_study, tags_care, tags_familySit, tags_employed, tags_ages, tags_additional, tags_income);
 
-                tagRepository.saveAll(result);
-            }
-            log.info("Tag 생성 완료");
+        for (int i = 0; i < array_tags.size(); i++) {
+            List<String> tags = array_tags.get(i);
+            Field field = fields.get(i);
 
-        }else {
-            log.info("Tag already exists");
+            List<Tag> result = tags.stream()
+                    .map(tag -> new Tag(tag, field))
+                    .collect(Collectors.toList());
+
+            tagRepository.saveAll(result);
         }
+        log.info("Tag 생성 완료");
+
         List<TagResponse> returnValue = getAllTagList();
         return returnValue;
     }
