ab67f0e3dd93d2f29860dbc3a32065ab4f6c0ce5
chaewon02 <glgl246@naver.com>
2025-07-19 17:16:23 +0900

    feat: FindIdResponse Dto 리스트 타입 변경

--- src/main/java/org/project/soar/model/user/dto/FindIdResponse.java
+++ src/main/java/org/project/soar/model/user/dto/FindIdResponse.java
@@ -3,12 +3,14 @@ package org.project.soar.model.user.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @NoArgsConstructor
 public class FindIdResponse {
-    private String userEmail;
+    private List<String> emails;
 
-    public FindIdResponse(String userEmail) {
-        this.userEmail = userEmail;
+    public FindIdResponse(List<String> userEmails) {
+        this.emails = userEmails;
     }
 }
